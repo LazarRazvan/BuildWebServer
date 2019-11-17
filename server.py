4df8235317577faaaf7576ca1cd0c87f619f1deb
LazarRazvan <grigore.razvan.lazar@gmail.com>
2019-11-17 22:54:20 +0200

    Server: add influxdb and hash function

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import os
+import sys
+import hashlib
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
+from influxdb import InfluxDBClient
 
 UPLOAD_FOLDER = '/tmp/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+CLIENT = None
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -44,6 +48,7 @@ def welcome():
     """
     return render_template("index.html");
 
+#LOGIN
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """
@@ -52,13 +57,28 @@ def login():
     error = None
     print ("login function is called")
     if request.method == 'POST':
-        print (request.data)
+        user = request.form.get('username')
+        pswd = request.form.get('password')
+
+        if "email" in request.form:
+            email = request.form.get('email')
+
+        print (create_hash(user, pswd).hexdigest())
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect('/build?')
 
     return render_template('login.html', error=error)
+
+#ABOUT
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    """
+    This function is called when about page is opened.
+    """
+    print ("About function is called")
+    return render_template("about.html")
 
 @app.route('/build', methods=['GET', 'POST'])
 def build():
@@ -70,18 +90,43 @@ def build():
         print ("request_form = " + request.form)
     return render_template("build.html");
 
-@app.route('/about', methods=['GET', 'POST'])
-def about():
-    """
-    This function is called when about page is opened.
-    """
-    print ("About function is called")
-    return render_template("about.html")
-
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
+def start_database(host_name, port_nr, db_name):
+    """
+    Start InfluxDB where authentification data will be stored
+    host_name:  hostname where InfluxDB will run
+    port_nr :   port used by InfluxDB
+    db_name :   database name to store users
+    """
+    global CLIENT
+    CLIENT = InfluxDBClient(host=host_name, port=port_nr)
+
+    if not CLIENT:
+        sys.exit("Fail to start InfluxDB on host %s and port %d" % (host_name, port_nr))
+
+    # Check if database is created. If not, create it
+    databases_dict = CLIENT.get_list_database()
+    databases = [d['name'] for d in databases_dict if 'name' in d]
+    if db_name not in databases:
+        CLIENT.create_database(db_name)
+
+    CLIENT.switch_database(db_name)
+
+def create_hash(username, password):
+    """
+    Create a hash to be used by server to identify a user
+    username    : User name
+    password    : User password
+    """
+    string = ('%s%s' % (username, password)).encode('utf-8')
+    hash = hashlib.md5()
+    hash.update(string)
+    return hash
+
 if __name__ == "__main__":
+    start_database('localhost', 8086, 'users')
     app.run(host="0.0.0.0")
