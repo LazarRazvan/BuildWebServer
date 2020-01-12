096762da7ee730d39cc58600469e2957fbdac2cb
LazarRazvan <grigore.razvan.lazar@gmail.com>
2020-01-12 18:53:58 +0200

    docker-compose: Integrate clang compiler service
    Create separate volumes for each compiler builder. Both services post
    the results in the same volume

--- clang-builder/poll.sh
+++ clang-builder/poll.sh
@@ -4,23 +4,31 @@
 # SLP secondis inside DIR and check if any file of extension EXT exists.
 
 # Set the directory and extension to look for
-DIR=$1
-EXT=$2
-SLP=$3
-LOGS="/var/log/poll/logs"
+DIR_SRC=$1
+DIR_RES=$2
+DIR_LOG=$3
+EXT=$4
+SLP=$5
 
 # Go to directory
-cd $DIR
+cd $DIR_SRC
 
-echo "Directory is changed"
 # Poll for changes
 while true; do
 	files=$(find . -name "*.$EXT")
-	echo "Do while ... "
 	if [ ! -z "$files" ]
 	then
 		current_date_time="`date "+%Y-%m-%d %H:%M:%S"`";
-		echo -e "[$current_date_time] Files detected:\n========\n$files\n========\n" >> $LOGS
+		echo -e "[$current_date_time] Files detected:\n========\n$files\n========" >> $DIR_LOG
+		for file in $files; do
+			# Get only basename
+			filename=$(basename $file)
+			/usr/local/bin/build.sh $filename $DIR_SRC $DIR_RES
+			err=$?
+			if [ $err -ne 0 ]; then
+				echo -e "[ERROR] : build.sh failed on target $filename\n" >> $DIR_LOG
+			fi
+		done
 	fi
 	sleep $SLP
 done
